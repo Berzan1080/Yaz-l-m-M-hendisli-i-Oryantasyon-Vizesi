eb0dafd045ca8593cd9f06c8d76e0a2411be05c8
Berzan Baran <berzanbaran.401@gmail.com>
2024-12-04 23:19:06 +0300

    Create Soru2.java

---
+++ Soru2.java
@@ -0,0 +1,8 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int n = 500; // Son sayı
+        int sum = n * (n + 1) / 2; // Toplam formülü
+
+        System.out.println("1'den 500'e kadar olan tamsayıların toplamı: " + sum);
+    }
+}
