995fc5ebb5add78ce7d35be7b145811530478f02
Berzan Baran <berzanbaran.401@gmail.com>
2024-12-04 23:19:49 +0300

    Create Soru3.java

---
+++ Soru3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; // Kullanıcıdan giriş almak için Scanner sınıfı
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Scanner nesnesi oluştur
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt(); // Kullanıcının girdiği sayıyı al
+
+        // Sayıyı kontrol et
+        if (number > 0) {
+            System.out.println("Girdiğiniz sayı pozitiftir.");
+        } else if (number < 0) {
+            System.out.println("Girdiğiniz sayı negatiftir.");
+        } else {
+            System.out.println("Girdiğiniz sayı sıfırdır.");
+        }
+        
+        scanner.close(); // Kaynakları serbest bırak
+    }
+}
